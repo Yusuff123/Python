59a9e40aec74859d3471f019fa4b7ebd1e0c8813
Yusuf <99141854+Yusuff123@users.noreply.github.com>
2022-02-07 19:37:39 +0300

    Add files via upload

---
+++ parolaoluşturucu.py
@@ -0,0 +1,23 @@
+harf1='A'
+harf2='b'
+harf3='c'
+harf4='d'
+harf5='e'
+harf6='F'
+harf7='r'
+harf8='d'
+harf9='h'
+harf10='k'
+harf11='u'
+harf12='o'
+harf13='q'
+harf14='z'
+
+import random
+x=random.randint(1, 999)
+y=random.randint(1, 500)
+z=random.randint(1, 300)
+t=random.randint(1, 200)
+s=random.randint(1, 600)
+sayi=0
+print(x,harf1,y,harf2,z,harf3,t,harf4,s)
